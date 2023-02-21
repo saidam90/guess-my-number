3528a34d814a4f023417046b6c01bb2b42ea9d15
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-02-21 21:05:37 +0400

    added game logic

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 28;
-document.querySelector('.score').textContent = 100;
-document.querySelector('.guess').value = 50;
+// document.querySelector('.number').textContent = 28;
+// document.querySelector('.score').textContent = 100;
+// document.querySelector('.guess').value = 50;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '😕 No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
