2d2cf720ddd0b979ed6e20f14b5ee900dba7c90a
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-02-21 18:53:09 +0400

    DOM manipulations

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 28;
+document.querySelector('.score').textContent = 100;
+document.querySelector('.guess').value = 50;
