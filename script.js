f0e451fcda8dce35a709f434b7b5c45e85608895
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-02-22 19:25:09 +0400

    refactoring

--- script.js
+++ script.js
@@ -27,23 +27,15 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '💥 You lost!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
       document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost!';
-      document.querySelector('.score').textContent = 0;
     }
   }
 });
@@ -60,3 +52,21 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '📈 Too high!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '💥 You lost!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '📉 Too low!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '💥 You lost!';
+//     document.querySelector('.score').textContent = 0;
+//   }
